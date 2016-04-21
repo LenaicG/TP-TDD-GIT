14cba2e8f6155d921c0cedc2b96da5753c207024
Lenaic Gillet <gillet.lenaic@gmail.com>
2016-04-21 15:43:58 +0200

    https://github.com/LenaicG/TP-TDD-GIT.git

--- RomanNumerals/src/main/java/iut/tdd/RomanNumerals.java
+++ RomanNumerals/src/main/java/iut/tdd/RomanNumerals.java
@@ -3,9 +3,21 @@ package iut.tdd;
 public class RomanNumerals {
 	public String convertToRoman(String arabe) {
 		String resultat="";
-		for(int i=0;i<Integer.valueOf(arabe); i++){
+		int valeur=Integer.valueOf(arabe);
+		if(valeur>=5){
+			resultat="V";
+			valeur= valeur-5;
+		}
+		if(valeur==4){
+			return "IV";
+		}
+		if(valeur<4){
+		for(int i=0;i<valeur;i++)	{	
 			resultat = resultat +"I";
+		
+			}
 		}
 		return resultat;
 	}
+
 }
--- RomanNumerals/src/test/java/iut/tdd/RomanNumeralsTest.java
+++ RomanNumerals/src/test/java/iut/tdd/RomanNumeralsTest.java
@@ -28,4 +28,40 @@ public class RomanNumeralsTest {
 		//Then
 		Assert.assertEquals(expected, actual);
 	}
+	
+	@Test
+	public void should_return_IV_when_4() {
+		//Given
+		String input = "4";
+		String expected = "IV";
+		RomanNumerals romanNumeral = new RomanNumerals();
+		//When
+		String actual = romanNumeral.convertToRoman(input);
+		//Then
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void should_return_V_when_5() {
+		//Given
+		String input = "5";
+		String expected = "V";
+		RomanNumerals romanNumeral = new RomanNumerals();
+		//When
+		String actual = romanNumeral.convertToRoman(input);
+		//Then
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void should_return_VII_when_7() {
+		//Given
+		String input = "7";
+		String expected = "VII";
+		RomanNumerals romanNumeral = new RomanNumerals();
+		//When
+		String actual = romanNumeral.convertToRoman(input);
+		//Then
+		Assert.assertEquals(expected, actual);
+	}
 }
